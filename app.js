1c7f250b33eac1711952aa2ae59af7c50ed9d2f3
sniperkla <sniperkla@gmail.com>
2026-01-04 20:37:42 +0700

    add more security safer

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(mongoSanitize()) // Data sanitization against NoSQL query injection
 
 // Rate Limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again later'
 })
